73dce2a31b40b6c655167a09573d0ae01429fdaa
IsabelGuMo <gm.isabel@gmail.com>
2023-02-22 15:51:50 +0100

    Create front structure

---
+++ src/components/HousesComponent/HousesComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HousesComponent = () => {
+  return (
+    <div>HousesComponent</div>
+  )
+}
+
+export default HousesComponent
---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavbarComponent = () => {
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-between header-container">
+      <div className="logo-text d-flex align-items-center">
+        <div className="groot-container">
+          {/* <img src={GrootLogo} alt='Groot logo' className='groot'/> */}
+        </div>
+        <h1 className="header-text">CaixaBankApp21</h1>
+      </div>
+      
+    </div>
+    </div>
+  )
+}
+
+export default NavbarComponent
