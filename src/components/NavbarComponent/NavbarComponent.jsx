955fd1dac1a31c74ecf71af726de6f6466c18743
DianaB177 <anaidb77@gmail.com>
2023-02-22 20:54:47 +0100

    update

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const NavbarComponent = () => {
   return (
@@ -6,20 +6,18 @@ const NavbarComponent = () => {
       <div className="d-flex align-items-center justify-content-between header-container p-4">
         <div className="d-flex flex-row align-items-center gap-3">
           <i className="fa-solid fa-bars h1 text-white"></i>
-          <img src={Logo} alt='logo' />
-          <NavLinkAtom class='fs-5' text='COMPRAR'/>
-          <NavLinkAtom class='fs-5' text='ALQUILER'/>
+          <img src={Logo} alt="logo" />
+          <NavLinkAtom class="fs-5" text="COMPRAR" />
+          <NavLinkAtom class="fs-5" text="ALQUILER" />
         </div>
-      
+
         <div className="w-30">
-          <NavLinkAtom class='fw-bold' text='Publica tu anuncio'/> 
+          <NavLinkAtom class="fw-bold" text="Publica tu anuncio" />
         </div>
         <h1 className="header-text">CaixaBankApp21</h1>
       </div>
-      
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
