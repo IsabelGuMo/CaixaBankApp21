2ba6c31dec45057361eeeca123ebd701b5e49bd4
IsabelGuMo <gm.isabel@gmail.com>
2023-02-22 20:29:01 +0100

    Finish navbar and create Router.js

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import '../../styles/Styles.css';
+import Logo from '../../assets/images/logoHygge.png';
+import NavLinkAtom from '../../atoms/buttons/NavLinkAtom';
 
 
 const NavbarComponent = () => {
   return (
     <div>
-      <div className="d-flex align-items-center justify-content-between header-container">
-      <div className="logo-text d-flex align-items-center">
-        <div className="">
-          {/* <img src={GrootLogo} alt='Groot logo' className='groot'/> */}
+      <div className="d-flex align-items-center justify-content-between header-container p-4">
+        <div className="d-flex flex-row align-items-center gap-4 w-75">
+          <i className="fa-solid fa-bars h1 text-white"></i>
+          <img src={Logo} alt='logo' />
+          <NavLinkAtom class='fs-5' text='COMPRAR'/>
+          <NavLinkAtom class='fs-5' text='ALQUILER'/>
         </div>
-        <h1 className="header-text">CaixaBankApp21</h1>
-      </div>
       
+        <div className="w-30">
+          <NavLinkAtom class='fw-bold' text='Publica tu anuncio'/> 
+        </div>
+      </div>
     </div>
-    </div>
+    
   )
 }
 
