b5575378121e2a18d2951d2e91f5ef31fc9b7f3d
IsabelGuMo <gm.isabel@gmail.com>
2023-02-22 20:47:51 +0100

    Finish navbar

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -8,7 +8,7 @@ const NavbarComponent = () => {
   return (
     <div>
       <div className="d-flex align-items-center justify-content-between header-container p-4">
-        <div className="d-flex flex-row align-items-center gap-4 w-75">
+        <div className="d-flex flex-row align-items-center gap-3">
           <i className="fa-solid fa-bars h1 text-white"></i>
           <img src={Logo} alt='logo' />
           <NavLinkAtom class='fs-5' text='COMPRAR'/>
