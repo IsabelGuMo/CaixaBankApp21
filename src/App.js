f9c5d78c83263002cdeb7a685e6b3102e4ee0d9f
DianaB177 <anaidb77@gmail.com>
2023-02-24 00:29:55 +0100

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import './App.css';
-import NavbarComponent from './components/NavbarComponent/NavbarComponent.jsx';
-import Formulary from './components/Formulary';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LandingPageComponent from './components/LandingPageComponent/LandingPageComponent';
-import DetailBy from './pages/DetailPage/DetailBy';
-import DetailRent from './pages/HomePage/DeatailRent';
-import DetailPage from './pages/DetailPage/DetailPage';
-
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent/NavbarComponent.jsx";
+import Formulary from "./components/Formulary";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPageComponent from "./components/LandingPageComponent/LandingPageComponent";
+import DetailBy from "./pages/DetailPage/DetailBy";
+import DetailRent from "./pages/DetailsRent/DetailRent";
+import DetailPage from "./pages/DetailPage/DetailPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavbarComponent/>
+        <NavbarComponent />
         <Routes>
-        <Route index element={<LandingPageComponent/>} />
-        <Route path='/detailPage' element={<DetailPage/>}/>
-        <Route path='/detailBy' element={<DetailBy/>}/>
-        <Route path='/detailRent' element={<DetailRent/>}/>
-        <Route path='/fomulary' element={<Formulary/>}/>
-
-      </Routes>
+          <Route index element={<LandingPageComponent />} />
+          <Route path="/detailPage" element={<DetailPage />} />
+          <Route path="/detailBy" element={<DetailBy />} />
+          <Route path="/detailRent" element={<DetailRent />} />
+          <Route path="/fomulary" element={<Formulary />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import '../../styles/Styles.css';
-import Logo from '../../assets/images/logoHygge.png';
-import { Link, NavLink} from 'react-router-dom';
-
+import React from "react";
+import "../../styles/Styles.css";
+import Logo from "../../assets/images/logoHygge.png";
+import { Link, NavLink } from "react-router-dom";
 
 const NavbarComponent = () => {
   return (
     <div>
       <div className="d-flex align-items-center justify-content-between header-container p-4">
         <div className="d-flex flex-row align-items-center gap-4 w-75">
-          <NavLink as={Link} to="/"><img src={Logo} alt='logo' /></NavLink>
-          <NavLink className='linkNav' as={Link} to="detailBy">COMPRAR</NavLink>
-          <NavLink className='linkNav' as={Link} to="detailRent">ALQUILAR</NavLink>
-          
+          <NavLink as={Link} to="/">
+            <img src={Logo} alt="logo" />
+          </NavLink>
+          <NavLink className="linkNav" as={Link} to="detailBy">
+            COMPRAR
+          </NavLink>
+          <NavLink className="linkNav" as={Link} to="detailRent">
+            ALQUILAR
+          </NavLink>
         </div>
-      
+
         <div className="w-30">
-          <NavLink className='linkNav' as={Link} to="formulary">ANUNCIATE</NavLink> 
+          <NavLink className="linkNav" as={Link} to="formulary">
+            ANUNCIATE
+          </NavLink>
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
