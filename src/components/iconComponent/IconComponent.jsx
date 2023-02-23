50e758e7d95575d54511f9fe735235a7023e8dd7
IsabelGuMo <gm.isabel@gmail.com>
2023-02-23 14:07:49 +0100

    Add icons

--- src/components/iconComponent/IconComponent.jsx
+++ src/components/iconComponent/IconComponent.jsx
@@ -6,48 +6,37 @@ const IconComponent = () => {
   return (
     <div className="container">
       <div className="col">
-        <div className="row-12">
-          <div className="container-fluid">
-            <form className="d-flex" role="search">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-                onChange=""
-              />
-              <button
-                className="btn btn-outline-danger rounded-end-circle"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
+        <div className="row-12 d-flex flex-row">
+          <div className="">
+            
           </div>
-          <div className="col d-flex">
-          <div className="row-6">
+          <div className="col">
+            <div className="row-6">
               <img src={pool} className="imgDetail" alt="imgage pool" />
-              <div>
+              <div className="row-12 d-flex flex-row">
                 <img src={pool} className="imgDetailSmall" alt="imgage pool" />
                 <img src={pool} className="imgDetailSmall" alt="imgage pool" />
               </div>
             </div>
           </div>
-          <div className="row-6 text">
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              asperiores? Illum enim rerum tenetur perspiciatis autem omnis
-              reprehenderit, velit culpa ratione facere. Architecto, ipsum
-              sapiente corporis asperiores quidem illum minus! Lorem ipsum dolor
-              sit, amet consectetur adipisicing elit. Harum, asperiores? Illum
-              enim rerum tenetur perspiciatis autem omnis reprehenderit, velit
-              culpa ratione facere. Architecto, ipsum sapiente corporis
-              asperiores quidem illum minus!
-            </p>
-          </div>
+            <div className="row-6 text">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
+                asperiores? Illum enim rerum tenetur perspiciatis autem omnis
+                reprehenderit, velit culpa ratione facere. Architecto, ipsum
+                sapiente corporis asperiores quidem illum minus! Lorem ipsum dolor
+                sit, amet consectetur adipisicing elit. Harum, asperiores? Illum
+                enim rerum tenetur perspiciatis autem omnis reprehenderit, velit
+                culpa ratione facere. Architecto, ipsum sapiente corporis
+                asperiores quidem illum minus!
+              </p>
+            </div>
           </div>
         </div>
-        <div className="row-3">
+        <div className="d-flex justify-content-around">
+          <i class="fa-solid fa-toilet"></i>
+          <i class="fa-regular fa-square"></i>
+          <i class="fa-solid fa-chart-area"></i>
           <p>275.000€</p>
         </div>
       </div>
