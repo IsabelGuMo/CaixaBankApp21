5610bed955041c9c288f44cb8a4e27265f5df5b2
DianaB177 <anaidb77@gmail.com>
2023-02-24 08:32:48 +0100

    cleaning

--- src/components/LandingPageComponent/LandingPageComponent.jsx
+++ src/components/LandingPageComponent/LandingPageComponent.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "../../styles/Styles.css";
-//import SearchAtom from "../../atoms/SearchAtom/SearchAtom";
-import "../../styles/Styles.css";
 import "./LandingPageComponent.css";
 
 const LandingPageComponent = () => {
   return (
     <div className="background">
-      {/* <SearchAtom/> */}
       <h1 className="text-center title">Encuentra tu hogar</h1>
       <h3 className="text-center subtitle">Somo especialistas en encontrar tu Hygge exterior</h3>
     </div>
