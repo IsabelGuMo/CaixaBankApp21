bb20b2aedd1b1deabde6dfccc2fe1e23717b351c
DianaB177 <anaidb77@gmail.com>
2023-02-24 07:24:25 +0100

    title do

--- src/components/LandingPageComponent/LandingPageComponent.jsx
+++ src/components/LandingPageComponent/LandingPageComponent.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import SearchAtom from "../../atoms/SearchAtom/SearchAtom";
+import "../../styles/Styles.css";
+//import SearchAtom from "../../atoms/SearchAtom/SearchAtom";
 import "../../styles/Styles.css";
 import "./LandingPageComponent.css";
 
 const LandingPageComponent = () => {
   return (
     <div className="background">
-      <SearchAtom/>
+      {/* <SearchAtom/> */}
+      <h1 className="text-center title">Encuentra tu hogar</h1>
+      <h3 className="text-center subtitle">Somo especialistas en encontrar tu Hygge exterior</h3>
     </div>
   );
 };
